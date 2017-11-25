4fc049b84b953ecb74f1d1792ce4ffc78bbe4a1c
abnerwang0991 <abnerwang0991>
2017-11-25 17:36:45 +0800

    update

--- js/main.js
+++ js/main.js
@@ -4,30 +4,42 @@ angular.module('myApp', []).controller('userCtrl', function($scope) {
   $scope.passw1 = '';
   $scope.passw2 = '';
   $scope.users = [
-  {id:0, lName:'劉', fName:"備" },
-  {id:1, lName:'關',  fName:"羽" },
-  {id:2, lName:'張',  fName:"飛" },
-  {id:3, lName:'諸葛', fName:"亮" },
-  {id:4, lName:'趙', fName:"雲" },
-  {id:5, lName:'龐',fName:"統" }
+	  {lName:'劉', fName:"備" },
+	  {lName:'關',  fName:"羽" },
+	  {lName:'張',  fName:"飛" },
+	  {lName:'諸葛', fName:"亮" },
+	  {lName:'趙', fName:"雲" },
+	  {lName:'龐',fName:"統" }
   ];
   $scope.edit = true;
   $scope.error = false;
   $scope.incomplete = false; 
   $scope.hideform = true; 
-  $scope.editUser = function(id) {
+  $scope.editUser = function(id,index) {
     $scope.hideform = false;
     if (id == 'new') {
       $scope.edit = true;
       $scope.incomplete = true;
       $scope.lName = '';
       $scope.fName = '';
-      } else {
+    } else {
       $scope.edit = false;
-      $scope.lName = $scope.users[id].lName;
-      $scope.fName = $scope.users[id].fName; 
+      $scope.lName = $scope.users[index].lName;
+      $scope.fName = $scope.users[index].fName; 
     }
   };
+
+  $scope.add = function() {
+   	  $scope.users.forEach(function(e, i) {
+        if(e.lName == $scope.lName && e.fName == $scope.fName) {
+          $scope.users.splice(i, 1);
+        }
+      });
+   	  $scope.users.push({
+      	lName: $scope.lName,
+        fName: $scope.fName
+      });
+    };
 
   $scope.$watch('passw1',function() {$scope.test();});
   $scope.$watch('passw2',function() {$scope.test();});
